1c7ac85bc50a4827cba00c86c1cae6de198cff0a
Alvin Wang <alvinwang922@gmail.com>
2020-12-23 21:18:04 -0800

    Implement K-nearest neighbors algorithm to classify cars

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -6,7 +6,6 @@ import numpy as np
 from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("car.data")
-print(data.head())
 
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data["buying"]))
@@ -17,7 +16,26 @@ lug_boot = le.fit_transform(list(data["lug_boot"]))
 safety = le.fit_transform(list(data["safety"]))
 cls = le.fit_transform(list(data["class"]))
 
-predict = "class"  # optional
+predict = "class"
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety))
+y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+model = KNeighborsClassifier(n_neighbors=7)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 7, True)
+    print("N: ", n)
 
 X = list(zip(buying, maint, door, persons, lug_boot, safety))
 y = list(cls)
