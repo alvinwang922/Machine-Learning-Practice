5a6b305f2732d03385a4f02f0206680f1e172636
Alvin Wang <alvinwang922@gmail.com>
2020-12-25 17:11:37 -0800

    Implement SVM to predict whether breast cancer is malignant or benign.

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -6,16 +6,16 @@ from sklearn.neighbors import KNeighborsClassifier
 
 cancer = datasets.load_breast_cancer()
 
-# print(cancer.feature_names)
+print(cancer.feature_names)
 # print(cancer.target_names)
 
 x = cancer.data
 y = cancer.target
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-
-clf = svm.SVC(kernel="linear", C=1)
+# kernel is function to add dimensions to data, C is soft margin
+clf = svm.SVC(kernel="linear", C=1.5)
 clf.fit(x_train, y_train)
 
 y_pred = clf.predict(x_test)
